ed845c731a0600f003461fb65a9e1da9848c699c
Yasith <yasithnawodya9@gmail.com>
2024-03-02 18:52:36 +0530

    add cors policy for backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,23 @@ const dotEnv = require('dotenv');
 require('dotenv').config();
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const Book = require("./model/bookmodel.js");
 const { getAllBookData, getSpesificBook, addNewBook, updateBook, deleteBook } = require('./routes/bookRoutes')
 
 
 app.use(express.json())
+
+//middleware to handle cors policy
+//1.all origins default with cors
+//app.use(cors())
+//2.allow only cuttom origin
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowHeaders: ['Content-Type']
+}))
 
 const PORT = process.env.PORT || 8070;
 const URL = process.env.URL
